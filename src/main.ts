8f4710f541145ff2c69ee6088fe4c06a8e553df4
大诚专用 <83280028+ChenCheng19981014@users.noreply.github.com>
2024-08-01 10:45:33 +0800

    fix:使用electron-vite创建

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import electron from "vite-plugin-electron";
-
-const app = createApp(App);
-
-app.mount("#app");
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+// TODO: 版本信息
+const versions = reactive({ ...window.electron.process.versions })
+
+// TODO: 向主程序发送消息
+
+// const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+console.log(' versions :', versions)
+</script>
+
+<template>{{ versions }}</template>
